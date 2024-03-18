b4f9e385aa32eb538e00b98fd04bb45fcc4a8853
chusakh <163238363+chusakh@users.noreply.github.com>
2024-03-18 13:30:23 +0700

    Update cloudnexxa.sh

--- cloudnexxa.sh
+++ cloudnexxa.sh
@@ -1,3 +1,5 @@
 curl -O https://network-gateway.openvpn.com/network-gate/api/v1/scripts/VWJ1bnR1IDIyLjA0/host/ubuntu_22_04.sh
 chmod +x ubuntu_22_04.sh
 ./ubuntu_22_04.sh
+
+# it require token like this: vPjQsOsIkiLGwIcr/76C9t+sCyPAPR7JEBEIfoiQb8E=5cf57e4978637c93f993fbfc2098db0becab0473
