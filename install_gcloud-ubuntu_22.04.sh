af1814ac11c61f809a381d6d98348e2e77fa9f6d
chusakh <163238363+chusakh@users.noreply.github.com>
2024-03-18 14:46:11 +0700

    Update install_gcloud-ubuntu_22.04.sh

--- install_gcloud-ubuntu_22.04.sh
+++ install_gcloud-ubuntu_22.04.sh
@@ -1,3 +1,9 @@
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates gnupg curl
+sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update
+sudo apt-get install google-cloud-cli
 sudo apt-get install google-cloud-cli-anthos-auth
 sudo apt-get install google-cloud-cli-app-engine-go
 sudo apt-get install google-cloud-cli-app-engine-grpc
@@ -23,3 +29,4 @@ sudo apt-get install google-cloud-cli-spanner-emulator
 sudo apt-get install google-cloud-cli-terraform-validator
 sudo apt-get install google-cloud-cli-tests
 sudo apt-get install kubectl
+sudo gcloud init
